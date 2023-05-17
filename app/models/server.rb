09491eaf61e22e308f34995d05e844b477722ed8
Yves Siegrist <Elektron1c97@gmail.com>
2023-05-17 20:45:07 +0200

    Introduce to_kubernetes_resource
    This method converts our DB record to a PrismServer (CRD) object which will make
    sure all the settings that the user entered will be configured in the actual server.

--- app/models/server.rb
+++ app/models/server.rb
@@ -10,4 +10,33 @@ class Server < ApplicationRecord
   enum :tickrate, "64": 64, "85": 85, "100": 100, "128": 128
   enum :game_type, classic: 0, gun_game: 1, training: 2, custom: 3, cooperative: 4, skirmish: 5, free_for_all: 6
   enum :game_mode, casual: 0, competitive: 1, wingman: 2, weapons_expert: 3
+
+  def kubernetes_name
+    "prism-web-#{id}"
+  end
+
+  def to_kubernetes_resource
+    Kubeclient::Resource.new(
+      metadata: {
+        name: kubernetes_name,
+        namespace: Rails.application.config.kubenamespace
+      },
+      spec: {
+        customer: "user-#{user.id}",
+        subscriptionStart: Time.now.to_i,
+        env: [
+          { name: "CSGO_HOSTNAME", value: name },
+          { name: "CSGO_GSLT", value: gslt },
+          { name: "CSGO_MAP", value: map },
+          { name: "CSGO_PW", value: password },
+          { name: "CSGO_RCON_PW", value: rcon_password },
+          { name: "CSGO_TICKRATE", value: tickrate.to_s },
+          { name: "CSGO_GAME_TYPE", value: read_attribute_before_type_cast(:game_type).to_s },
+          { name: "CSGO_GAME_MODE", value: read_attribute_before_type_cast(:game_mode).to_s },
+          { name: "CSGO_DISABLE_BOTS", value: disable_bots.to_s },
+          { name: "SERVER_CONFIGS", value: server_configs.to_s },
+        ]
+      }
+    )
+  end
 end
--- test/models/server_test.rb
+++ test/models/server_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class ServerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "to_kubernetes_resource" do
+    fixed_timestamp = 1672527600
+    travel_to Time.at(fixed_timestamp) do
+      assert_equal(
+        {
+          metadata: {
+            name: "prism-web-#{servers(:black_pearl).id}",
+            namespace: Rails.application.config.kubenamespace
+          },
+          spec: {
+            customer: "user-#{users(:jack).id}",
+            subscriptionStart: fixed_timestamp,
+            env: [
+              { name: "CSGO_HOSTNAME", value: "Black Pearl Gaming Inc." },
+              { name: "CSGO_GSLT", value: "12345678" },
+              { name: "CSGO_MAP", value: "de_inferno" },
+              { name: "CSGO_PW", value: "seltournament1234" },
+              { name: "CSGO_RCON_PW", value: "seladmin42$2$" },
+              { name: "CSGO_TICKRATE", value: "128" },
+              { name: "CSGO_GAME_TYPE", value: "0" },
+              { name: "CSGO_GAME_MODE", value: "1" },
+              { name: "CSGO_DISABLE_BOTS", value: "true" },
+              { name: "SERVER_CONFIGS", value: "true" },
+            ]
+          }
+        },
+        servers(:black_pearl).to_kubernetes_resource.to_h)
+    end
+  end
 end
